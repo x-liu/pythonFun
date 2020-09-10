de19ab456beb26204433b70dc251d7d6dd0c68f8
Xing Liu <xingliu321@gmail.com>
2020-09-09 19:22:36 -0500

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+print("Hello Python world!")
+myName = ['xing','liu']
+print(myName)
+myName[0] = 'Meina'
+message = f"My first name is {myName[0].title()}, and my last name is {myName[1].title()}"
+print(message)
